55ec7997fdfc4666588f47058dcccd760e683af0
Clemens Bergmann <cbergmann@schuhklassert.de>
2015-11-19 00:53:40 +0100

    fix problems

--- inwx.rb
+++ inwx.rb
@@ -7,17 +7,16 @@ module Proxy::Dns
   class Inwx < Record
     include Proxy::Log
     include Proxy::Util
-    attr_reader :resolver
     attr_accessor :domrobot
     attr_accessor :object
-    
+
     def initialize options = {}
-      user = Proxy::Dns::Plugin.settings.dns_user
-      pass = Proxy::Dns::Plugin.settings.dns_password
+      user = options[:user]
+      passwd = options[:pass]
       addr = "api.domrobot.com"
       @object = "nameserver"
       @domrobot = INWX::Domrobot.new(addr)
-      result = self.domrobot.login(user,pass)
+      result = self.domrobot.login(user,passwd)
       super(options)
     end
 
@@ -25,56 +24,43 @@ module Proxy::Dns
     # create({ :fqdn => "node01.lab", :value => "3.100.168.192.in-addr.arpa",
     #          :type => "PTR"}
     def create
-      @resolver = Resolv::DNS.new(:nameserver => @server)
       method = "createRecord"
       #case @type
       #when "A"
-      if ip = dns_find(@fqdn)
-        raise(Proxy::Dns::Collision, "#{@fqdn} is already used by #{ip}") unless ip == @value
+      ip, id = dns_find(@fqdn)
+      if ip
+        raise(Proxy::Dns::Collision, "#{@fqdn} is already used") unless ip == @value
       else
-        domain = @fqdn.sub(/[^.]+./,'')
-        params = { :domain => domain, :type => @type, :content => @value, :name => @fqdn }
+        domain, name = split_fqdn(@fqdn)
+        params = {:domain => domain, :type => @type, :content => @value, :name => name }
         result = self.domrobot.call(self.object, method, params)
-        msg = "Added DNS entry #{@fqdn} => #{@value}"
+        msg = "add #{@type} DNS entry #{name} => #{@value} to domain #{domain}"
         report msg, result["msg"], false
-      end 
+      end
       #end
     end
 
     # remove({ :fqdn => "node01.lab", :value => "192.168.100.2"}
     # remove({ :fqdn => "node01.lab", :value => "3.100.168.192.in-addr.arpa"}
     def remove
-      @resolver = Resolv::DNS.new(:nameserver => @server)
       #case @type
       #when "A"
-      raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{@fqdn}") unless dns_find(@fqdn)
-      msg = "Removed DNS entry #{@fqdn} => #{@value}"
-      method = "info"
-      domain = @fqdn.sub(/[^.]+./,'')
-      params = { :domain => domain, :name => @fqdn }
-      result = self.domrobot.call(self.object, method, params)
-      
+      ip, id = dns_find(@fqdn)
+      raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{@fqdn}") unless id
+      msg = "remove DNS entry #{@fqdn} => #{@value}"
       method = "deleteRecord"
-      ids = result["resData"]["record"]
-      if ids.nil?
-        report msg, "completed successfully: no DNS entry available to delete", false
-        return true
-      end
-
-      params = { :id => ids[0]["id"] }
+      params = { :id => id }
       result = self.domrobot.call(self.object, method, params)
-      
+
       report msg, result["msg"], false
       #end
     end
 
     private
-
+    
     def report msg, response, error_only=false
       if not response.include? "completed successfully"
         logger.error "Inwx failed:\n" + response.join("\n")
-        msg.sub! /Removed/,    "remove"
-        msg.sub! /Added/,      "add"
         msg  = "Failed to #{msg}"
         raise Proxy::Dns::Error.new(msg)
       else
@@ -87,14 +73,22 @@ module Proxy::Dns
       raise Proxy::Dns::Error.new("Unknown error while processing '#{msg}'")
     end
 
-    def dns_find key
-      if match = key.match(/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/)
-        resolver.getname(match[1..4].reverse.join(".")).to_s
-      else
-        resolver.getaddress(key).to_s
-      end
-    rescue Resolv::ResolvError
-      false
+    def split_fqdn fqdn
+      domain = fqdn.split('.')[-2..-1].join('.')
+      name = fqdn.chomp(".#{domain}")
+      [domain,name]
+    end
+
+    def dns_find fqdn
+      domain, name = split_fqdn(fqdn)
+      method = "info"
+      params = { :domain => domain, :name => name }
+      logger.error params.inspect
+      result = self.domrobot.call(self.object, method, params)
+      logger.error result.inspect
+      return false if result["resData"]["record"].nil?
+
+      return [ result["resData"]["record"][0]["content"], result["resData"]["record"][0]["id"] ]
     end
   end
 end
